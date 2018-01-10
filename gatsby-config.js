aca1a8fca8a30dc1823b528262f2f6505244edc6
Hays Clark <hays.clark@gmail.com>
2018-01-09 16:05:41 -0800

    fix: maximizing image width to 710px

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
           {
             resolve: "gatsby-remark-images",
             options: {
-              maxWidth: 690
+              maxWidth: 710
             }
           },
           {
