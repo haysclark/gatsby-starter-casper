d245dc1cdda8e2ae73f12fb878a6541b6d3a79a9
Hays Clark <hays.clark@gmail.com>
2018-03-07 13:46:41 -0800

    fix: fixing MainNav rendering
    Adding missing ‘overlay’ and ‘clearfix’ CSS classes to MainNav to resolve render issue when no Cover image is provided.
    Updating Big-Sample-Post to have no cover for validation
    
    Resolves #18

--- src/layouts/MainNav/MainNav.jsx
+++ src/layouts/MainNav/MainNav.jsx
@@ -4,11 +4,9 @@ import "./MainNav.css";
 
 class MainNav extends React.Component {
   render() {
-    const { children, overlay } = this.props;
-
-    const classes = classNames("main-nav", this.props.className, {
-      overlay
-    });
+    const { children, className } = this.props;
+    const formatting = ["overlay", "clearfix"];
+    const classes = classNames("main-nav", formatting, className);
 
     return <nav className={classes}>{children}</nav>;
   }
