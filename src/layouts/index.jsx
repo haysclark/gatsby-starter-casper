99c519a00d412eb4564433a4d0de21617996f261
hnspn <bobmatt911@gmail.com>
2018-10-09 18:32:55 -0400

    added import for prismjs css file to fix missing syntax highlighting

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Helmet from "react-helmet";
 import config from "../../data/SiteConfig";
 import "./index.css";
+import 'prismjs/themes/prism.css'
 
 export default class MainLayout extends React.Component {
   getLocalTitle() {
